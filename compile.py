529ca64b85fb9e10e17677d06d715ca9e6f6cec4
Austin Fell <austinrf2918@yahoo.com>
2016-05-07 23:29:50 -0400

    Added landing page, starting to stylize it.

--- compile.py
+++ compile.py
@@ -1,7 +1,12 @@
 import os
 
 compilePath = 'GlobalStylesheet/sass/app.scss'
-filePaths = ['LeadSnareLogin/css/app.css', 'LeadSnareSignUp/css/app.css', 'LeadSnareDashboardPeople/css/app.css']
+filePaths = ['LeadSnareLogin/css/app.css', 'LeadSnareSignUp/css/app.css', 'LeadSnareDashboardPeople/css/app.css', 'LeadSnareLandingPage/css/app.css']
 
-for file in filePaths:
-    os.system("sass " + compilePath + " " + file)
+x = input('Enter script or all');
+
+if (x == 'all'):
+    for file in filePaths:
+        os.system("sass " + compilePath + " " + file)
+else:
+    os.system("sass " + compilePath + " " + filePaths[int(x)])
