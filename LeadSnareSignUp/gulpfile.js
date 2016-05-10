b4a1d214259f5cd53f67a7b8ea5709923b552437
Austin Fell <austinrf2918@yahoo.com>
2016-05-09 22:27:20 -0400

    Added gulp files for live web development.

---
+++ LeadSnareSignUp/gulpfile.js
@@ -0,0 +1,38 @@
+var gulp = require('gulp');
+
+var EXPRESS_PORT = 4000;
+var EXPRESS_ROOT = __dirname;
+var LIVERELOAD_PORT = 35729;
+
+function startExpress(){
+    var express = require('express');
+    var app = express();
+    app.use(require('connect-livereload')());
+    app.use(express.static(EXPRESS_ROOT));
+    app.listen(EXPRESS_PORT);
+}
+
+var lr;
+
+function startLiveReload(){
+    lr = require('tiny-lr')();
+    lr.listen(LIVERELOAD_PORT);
+}
+
+function notifyLivereload(event){
+    var fileName = require('path').relative(EXPRESS_ROOT, event.path);
+    console.log("Change.");
+
+    lr.changed({
+        body: {
+        files: [fileName]
+    }
+    })
+}
+
+gulp.task('default', function()
+{
+    startExpress();
+    startLiveReload();
+    gulp.watch(['*.html', 'css/*.css', 'js/**/*', 'js/*'], notifyLivereload);
+})
